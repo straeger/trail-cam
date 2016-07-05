e158bef28e9ef9a8868c6443c9c0c2b7f8c85517
Stephan <traeger.stephan@googlemail.com>
2016-07-05 19:50:27 +0200

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#motionSensor = gpio 21
+#motionSensor = gpio 19
 #relaus = gpio 16 20
 #ligth gpio 26
 
@@ -7,7 +7,6 @@ import RPi.GPIO as GPIO
 
 from MotionCaptureService import MotionCaptureService
 
-GPIO.setmode(GPIO.BOARD)
 motionCaptureService = MotionCaptureService(21)
 
 # you can continue doing other stuff here
